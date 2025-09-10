eda0baec5cfcf470178861766732a72abd930619
João Camargo <jclaudio00_12@hotmail.com>
2025-09-10 20:24:14 -0300

    added migrations tables companies, users, jobs, applications

---
+++ database/migrations/2025_09_10_231131_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('cnpj', 18)->unique();
+            $table->enum('plan', ['Free', 'Premium'])->default('Free');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
---
+++ database/migrations/2025_09_10_231200_create_jobs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->string('title', 150);
+            $table->text('description')->nullable();
+            $table->enum('job_type', ['Contractor', 'FullTime', 'Internship']);
+            $table->decimal('salary', 10, 2)->nullable();
+            $table->integer('work_hours')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
