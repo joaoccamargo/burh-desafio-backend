e00c8252f969d1cb8681a9cf63417c03e2b134e0
João Camargo <jclaudio00_12@hotmail.com>
2025-09-11 21:02:35 -0300

    added users, companies, jobs, applications endpoints

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Company\CompanyController;
+use App\Http\Controllers\Job\JobController;
+use App\Http\Controllers\Application\ApplicationController;
+
+// -----------------------------
+// Users
+// -----------------------------
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+// -----------------------------
+// Companies
+// -----------------------------
+Route::prefix('companies')->group(function () {
+    Route::get('/', [CompanyController::class, 'index']);
+    Route::post('/', [CompanyController::class, 'store']);
+    Route::get('/{id}', [CompanyController::class, 'show']);
+    Route::put('/{id}', [CompanyController::class, 'update']);
+    Route::delete('/{id}', [CompanyController::class, 'destroy']);
+});
+
+// -----------------------------
+// Jobs
+// -----------------------------
+Route::prefix('jobs')->group(function () {
+    Route::get('/', [JobController::class, 'index']);
+    Route::post('/', [JobController::class, 'store']);
+    Route::get('/{id}', [JobController::class, 'show']);
+    Route::put('/{id}', [JobController::class, 'update']);
+    Route::delete('/{id}', [JobController::class, 'destroy']);
+});
+
+// -----------------------------
+// Applications
+// -----------------------------
+Route::prefix('applications')->group(function () {
+    Route::get('/', [ApplicationController::class, 'index']);
+    Route::post('/', [ApplicationController::class, 'store']);
+    Route::get('/{id}', [ApplicationController::class, 'show']);
+    Route::delete('/{id}', [ApplicationController::class, 'destroy']);
+});
